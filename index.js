448adacd6e01bee5771bbf39b9b6bfe7e31877d3
Berian <chaiwa.berian@gmail.com>
2018-11-18 01:08:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ function* run() {
   if (commander.shell) {
     console.log(chalk.blue(`Running mongo shell: ${mongo}`));
     let shell_default_host = (hosts[0].split(':'))[0];
-    let default_port = (hosts[0].split(':'))[1];
+    let shell_default_port = (hosts[0].split(':'))[1];
     spawn(mongo, 
       isWin ? ['--quiet','--port', default_port, '--host', default_host] : ['--quiet'], 
       { stdio: 'inherit' }
